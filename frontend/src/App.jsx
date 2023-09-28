40fca6cb913c6d4de2eb7baab192ed4fd467cb6c
unknown <micaelaestrellamorales@gmail.com>
2023-09-28 11:54:01 -0300

    Create React Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link
+} from "react-router-dom"
+import HomePage from './homepage/HomePage'
+import './styles/App.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (<HomePage/>)
+    },
+]);
+
+function App() {
+    return (<RouterProvider router={router} />)
+}
+
+export default App;
